a0919cddea5b201b9389218a32f75d869443d1fd
mario.mariani <mariodantecuellar72@gmail.com>
2016-07-22 14:56:14 -0300

    Add Main class for execute program

--- InterviewSolution/0.CodeProject/Program.cs
+++ InterviewSolution/0.CodeProject/Program.cs
@@ -1,10 +1,21 @@
 ﻿
+using _0.CodeProject;
+using System;
+
 namespace CodeProject
 {
     class Program
     {
         static void Main(string[] args)
         {
+            JobLogger init = new JobLogger(true, true, true, true, true, true);
+
+            init.LogMessage("message1: Error", JobLogger.LogLevel.Error);
+
+            init.LogMessage("message2: Message", JobLogger.LogLevel.Message);
+            init.LogMessage("message3: Warning", JobLogger.LogLevel.Warning);
+
+            Console.ReadKey();
         }
     }
 }
