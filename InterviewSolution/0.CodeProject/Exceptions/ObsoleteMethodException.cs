b9379ac8dfd676a262c1a5da104fee64453835b0
mario.mariani <mariodantecuellar72@gmail.com>
2016-07-22 08:17:35 -0300

    Created an exception for obsolete method.

---
+++ InterviewSolution/0.CodeProject/Exceptions/ObsoleteMethodException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0.CodeProject.Exceptions
+{
+    class ObsoleteMethodException : Exception
+    {
+        public ObsoleteMethodException()
+        {
+
+        }
+
+        public ObsoleteMethodException(string message): base(message)
+        {
+
+        }
+
+    }
+
+}
