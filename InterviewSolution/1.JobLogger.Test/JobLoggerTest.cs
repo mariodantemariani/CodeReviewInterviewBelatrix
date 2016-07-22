b356763ec4c9c51c2997405d8bd5069a89082b06
mario.mariani <mariodantecuellar72@gmail.com>
2016-07-22 13:35:57 -0300

    Created test about logToDataBase

---
+++ InterviewSolution/1.JobLogger.Test/JobLoggerTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0.CodeProject;
+using System;
+
+namespace _1.JobLoggerTest
+{
+    
+    [TestClass]
+    public class JobLoggerTest
+    {
+        #region variables
+        private string msgToTest = "test";
+        #endregion
+
+
+        public JobLoggerTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+
+        #region tests about JobLog initialization
+
+        #endregion
+
+        #region tests about JobLog validation
+
+        #endregion
+
+        #region tests about JobLog method
+        
+        [TestMethod]
+        public void LogToDataBaseTest()
+        {
+            //Only LogDataBase test
+            JobLogger init = new JobLogger(false, false, true, true, true, true);
+
+            init.LogMessage(msgToTest, JobLogger.LogLevel.Error);
+
+            string expectedString = string.Format("{0} {1}", DateTime.Now.ToShortDateString(), msgToTest);
+            string actualString = expectedString; 
+
+            Assert.AreEqual(expectedString, actualString);
+        }
+
+       
+        #endregion
+
+
+    }
+}
